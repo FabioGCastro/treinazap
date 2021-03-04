bb1e3f085284542ff7aa5f4efacfaee5f75e3cd6
FabioGCastro <61331895+FabioGCastro@users.noreply.github.com>
2021-03-04 19:44:32 -0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase';	
 
 const firebaseConfig = {	
-    apiKey: "AIzaSyBxBgdDpZS9cVUE5FN0sEiX1yK4rKTnqjE",
-    authDomain: "treinazap2.firebaseapp.com",
-    projectId: "treinazap2",
-    storageBucket: "treinazap2.appspot.com",
-    messagingSenderId: "999974237133",
-    appId: "1:999974237133:web:478b78fdc88188a04cca84" 
+    apiKey: "${VAR_API}",
+    authDomain: "${VAR_AUTH}",
+    projectId: "${VAR_PROJECT}",
+    storageBucket: "${VAR_STORAGE}",
+    messagingSenderId: "${VAR_MESS}",
+    appId: "${VAR_APP}" 
 };	
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);	
